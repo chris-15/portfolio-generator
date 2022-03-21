393fc481c6811a1abf712e212f1b41639a71f9f9
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-20 21:44:51 -0400

    created promises and finished site

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const {writeFile, copyFile} =  require("./utils/generate-site");
 const generatePage = require("./src/page-template.js");
 
 const inquirer = require("inquirer");
@@ -143,10 +143,19 @@ const promptProject = (portfolioData) => {
 
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw err;
-        console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
